dda8d01f0bbd4139d1b4ef413dca72e0a1aa1cf5
David Rosario <david@lavacro.com>
2026-08-08 00:37:10 -0400

    deployed

--- src/main/groovy/net/lavacro/serverless/engine/services/SyslogService.java
+++ src/main/groovy/net/lavacro/serverless/engine/services/SyslogService.java
@@ -41,8 +41,6 @@ public class SyslogService {
 		String str = String.format("%s|%s|%s|%s",
 				data.get("hostname"), data.get("address"), data.get("port"), data.get("protocol"));
 
-		log.info("string: {}", str);
-
 		long[] hash = xxh128.hashBytes(str.getBytes(StandardCharsets.UTF_8));
 
 		// Convert the 128-bit hash (two longs) into 16 bytes for MongoDB _id
@@ -52,15 +50,11 @@ public class SyslogService {
 				.putLong(hash[1])
 				.array();
 
-		log.info("Hash bytes: {}", hashBytes);
-
 		String uuid = String.format("%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
 				hashBytes[0], hashBytes[1], hashBytes[2], hashBytes[3],
 				hashBytes[4], hashBytes[5], hashBytes[6], hashBytes[7],
 				hashBytes[8], hashBytes[9], hashBytes[10], hashBytes[11],
 				hashBytes[12], hashBytes[13], hashBytes[14], hashBytes[15]);
-
-		log.info("UUID: {}", uuid);
 
 		/*
 		 * data:
